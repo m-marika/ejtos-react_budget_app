9427ada0f87b7929d30bbf5976c9924fe1b9130b
m-marika1 <m-marika1@yandex>
2024-05-15 01:59:35 -0400

    final project done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { FaTimesCircle, FaPlusCircle } from 'react-icons/fa';
+import { FaTimesCircle, FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,12 +25,26 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency[0]}{props.cost}</td>
-        <td><FaPlusCircle name="circle_plus" onClick={event=> increaseAllocation(props.name)}/></td>
-        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteExpense}></FaTimesCircle></td>
+        <td><FaPlusCircle color="green" size='2em' onClick={event=> increaseAllocation(props.name)}/></td>
+        <td><FaMinusCircle color="red" size='2em' onClick={event=> decreaseAllocation(props.name)}/></td>
+        <td><FaTimesCircle onClick={handleDeleteExpense}></FaTimesCircle></td>
         </tr>
     );
 };
